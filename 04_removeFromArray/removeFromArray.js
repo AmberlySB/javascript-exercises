425c654c8e5187ef77ac755b89e830655af1ef19
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-13 03:41:13 -0500

    Javascript exercise 04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...moreArgs) {
+    for(i = 0; i < array.length; i++) {
+        for (j = 0; j < moreArgs.length; j++) {
+            if (moreArgs[j] === array[i]) {
+                array.splice(i, 1);
+                i = 0;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
