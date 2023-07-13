d4cb7248a1a0b67fd6edfa220667bc66ed7effc5
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-13 01:56:13 -0500

    Javascript exercise 02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return "ERROR";
+    }
+    else{
+        let myString = "";
+        for (i = 0; i < number; i++) {
+            myString += string;
+        }
+        return myString;
+    }
 };
 
 // Do not edit below this line
