cb5206fbaf54696312804f8fbadacfdf8e12b81a
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-13 02:27:46 -0500

    Javascript exercise 03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let stringResult = "";
+    for (i = 0; i < stringArray.length; i++) {
+        stringResult += stringArray[stringArray.length - 1 - i];
+    }
+    return stringResult;
 };
 
 // Do not edit below this line
