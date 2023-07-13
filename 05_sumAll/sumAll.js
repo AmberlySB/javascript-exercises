441a1947c6546fc2637d8d6d28f42454a9e1eb86
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-13 14:50:51 -0500

    Javascript exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (typeof numOne != "number" || typeof numTwo != "number"){
+        return "ERROR";
+    }
+    else if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
+    else {
+        let sum = 0;
+        if (numOne > numTwo) {
+            for (i = numTwo; i < (numOne + 1); i++) {
+                sum += i;
+            }
+        }
+        else {
+            if (numTwo > numOne) {
+                for (i = numOne; i < (numTwo + 1); i++) {
+                    sum += i;
+                }
+            }
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
